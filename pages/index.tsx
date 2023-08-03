6d1fbe8c9ef63e8c6b3d68237a344a3b3abccb02
Jhune Carlo Trogelio <jhunecarlo.trogelio@gmail.com>
2023-08-04 01:07:52 +0800

    feat: Added tailwindcss on the application

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function IndexPage() {
+  return (
+    <>
+      <Head>
+        <title> Jhune Carlo Trogelio</title>
+        <meta 
+          name="Jhune Carlo Trogelio"
+          content="Jhune Carlo's personal web portfolio"
+        />
+      </Head>
+      <h1 className="bg-slate-50"> INITIAL IMPLEMENTATION</h1>
+    </>
+  )
+}
